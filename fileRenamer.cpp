32af8af0873aa051025f5b795854d94675152f44
MESSIAH JESUS SON OF GABRIEL SLAVE OF SACRED KING SUPREME ELEGANT <ayyildizabdurrahman@gmail.com>
2023-04-03 01:16:21 +0300

    Add files via upload

---
+++ fileRenamer.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    string fileType, oldname, newname;
+    int startIndex, endIndex, increase;
+
+    cout << "Shalom To File Renamer" << endl
+         << "----------------------" << endl;
+    
+    cout << "Enter Type of Files To Be Renamed : ";
+    cin >> fileType;
+    
+    fileType = "." + fileType;
+
+    cout << "Enter Starting File Name Index Number: ";
+    cin >> startIndex;
+
+    cout << "Enter Ending File Name Index Number: ";
+    cin >> endIndex;
+
+    cout << "Enter How Much you want to increase each file name as a number: ";
+    cin >> increase;
+
+    for(int i = startIndex; i <= endIndex; ++i)
+    {
+        oldname = to_string(i) + fileType;
+        newname = to_string(increase + i) + fileType;
+
+        if(rename(oldname.c_str(), newname.c_str()) != 0)
+            cout << "Error occured while renaming " << i << fileType << endl;
+    }
+
+    return 0;
+}
